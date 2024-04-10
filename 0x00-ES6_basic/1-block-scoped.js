5b349f204f98d1a1f95c1043bdc865bb8d6ed5d2
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-10 16:05:49 +0000

    Update 1-block-scoped.js file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true; // Modify the existing 'task'
-    task2 = false; // Modify the existing 'task2'
+    // This block should not alter the original values of task and task2
+    const task = true; // This is a new 'task', scoped only within the if block
+    const task2 = false; // This is a new 'task2', scoped only within the if block
   }
 
   return [task, task2];
