85df6048b22f8c7d8def6a01fd11ce26b0195395
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-17 18:46:46 +0100

    Update React.ts file

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,7 +1,5 @@
-import { Subjects } from './Subject';
-
-export namespace Subjects {
-    export class React extends Subject {
+namespace Subjects {
+    export class React extends Subjects.Subject {
         getRequirements(): string {
             return "Here is the list of requirements for React";
         }
