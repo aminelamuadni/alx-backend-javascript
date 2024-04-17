05ca509cc01a5a72499bf693dbdfe4246b87aced
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-17 15:09:14 +0100

    Add 8-clean_set.js file

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+
+  let result = '';
+  for (const item of set) {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, -1); // Remove the trailing dash
+}
+
+export default cleanSet;
