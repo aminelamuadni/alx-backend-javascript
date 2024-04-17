e1af2f5a1a8f15b989b8f4cd25d39e91934ef08e
Amine Lamuadni <W7equwE5@gmail.com>
2024-04-17 18:32:06 +0100

    Add Cpp.ts file

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,16 @@
+import { Subjects } from './Subject';
+
+export namespace Subjects {
+    export class Cpp extends Subject {
+        getRequirements(): string {
+            return "Here is the list of requirements for Cpp";
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingC && this.teacher.experienceTeachingC > 0) {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+            return "No available teacher";
+        }
+    }
+}
