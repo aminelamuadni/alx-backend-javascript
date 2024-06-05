4788e1372005d901221b835dff7fef1eda617471
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-05 22:01:20 +0100

    Add 0-console.js file

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+// Function to display a message
+function displayMessage(message) {
+  console.log(message);
+}
+
+// Exporting the function to be used in other files
+module.exports = displayMessage;
