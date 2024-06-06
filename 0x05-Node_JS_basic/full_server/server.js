1c17da46b315e905c0c48b104ddff2d80104a8e3
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 15:31:32 +0100

    Add server.js file

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = 1245;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
+
+export default app;
+module.exports = app;
