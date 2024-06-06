a615a13f9058ae50cb02e6587bf698967717fc09
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 20:05:14 +0100

    Add api.test.js file

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('aPI integration tests', () => {
+  it('should return the correct result for the index page', () => new Promise((done) => {
+    request.get('http://localhost:7865/', (_err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  }));
+
+  describe('cart page', () => {
+    it('should return correct response when id is a number', () => new Promise((done) => {
+      request.get('http://localhost:7865/cart/12', (_err, res, body) => {
+        expect(res.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    }));
+
+    it('should return 404 when id is NOT a number', () => new Promise((done) => {
+      request.get('http://localhost:7865/cart/hello', (_err, res) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    }));
+  });
+});
