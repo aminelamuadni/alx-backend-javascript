a33881d3bf481b1a8a5d1cc6d0482487415a074c
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 16:23:25 +0100

    Update StudentsController.js file

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,32 +1,33 @@
-import { readDatabase } from '../utils';
+import readDatabase from '../utils';
 
 class StudentsController {
   static async getAllStudents(req, res) {
     try {
       const data = await readDatabase(process.argv[2]);
-      let response = 'This is the list of our students\n';
+      const response = ['This is the list of our students'];
       Object.keys(data).sort().forEach((field) => {
-        response += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
+        response.push(`Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}`);
       });
-      return res.status(200).send(response);
+      return res.status(200).send(response.join('\n')); // Ensure to use return here
     } catch (error) {
-      return res.status(500).send(error.message);
+      return res.status(500).send('Cannot load the database'); // Ensure to use return here
     }
   }
 
   static async getAllStudentsByMajor(req, res) {
-    const { major } = req.params;
-    if (!['CS', 'SWE'].includes(major)) {
-      return res.status(500).send('Major parameter must be CS or SWE');
-    }
     try {
+      const { major } = req.params;
+      if (!['CS', 'SWE'].includes(major)) {
+        return res.status(500).send('Major parameter must be CS or SWE'); // Ensured return is used here
+      }
+
       const data = await readDatabase(process.argv[2]);
       if (data[major]) {
-        return res.status(200).send(`List: ${data[major].join(', ')}`);
+        return res.status(200).send(`List: ${data[major].join(', ')}`); // Ensured return is used here
       }
-      return res.status(404).send('Major not found');
+      return res.status(404).send('Major not found'); // Ensured return is used here
     } catch (error) {
-      return res.status(500).send(error.message);
+      return res.status(500).send('Cannot load the database'); // Ensured return is used here
     }
   }
 }
