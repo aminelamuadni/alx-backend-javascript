5c8afe9480be466728aa443097485a9671cb9164
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 21:40:55 +0100

    Update utils.js file

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,38 +1,34 @@
 import fs from 'fs';
 
 const readDatabase = (path) => new Promise((resolve, reject) => {
-  if (!path) {
-    reject(new Error('Cannot load the database: file path is undefined'));
-  }
-  if (path) {
-    fs.readFile(path, (error, data) => {
-      if (error) {
-        reject(new Error('Cannot read the database file'));
-      }
-      if (data) {
-        const lines = data
-          .toString('utf-8')
-          .trim()
-          .split('\n');
-        const database = {};
-        const headers = lines[0].split(',');
-        const columnNames = headers.slice(0, headers.length - 1);
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
 
-        for (const line of lines.slice(1)) {
-          const values = line.split(',');
-          const rowData = values.slice(0, values.length - 1);
-          const category = values[values.length - 1];
-          if (!Object.keys(database).includes(category)) {
-            database[category] = [];
-          }
-          const record = columnNames
-            .map((header, index) => [header, rowData[index]]);
-          database[category].push(Object.fromEntries(record));
-        }
-        resolve(database);
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
+
+      const lines = data.trim().split('\n');
+      const database = {};
+      lines.shift();
+
+      lines.forEach((line) => {
+        if (line.trim().length === 0) return;
+        const [firstname, , , field] = line.split(',');
+        if (!database[field]) {
+          database[field] = [];
+        }
+        database[field].push(firstname.trim());
+      });
+
+      resolve(database);
     });
-  }
+  });
 });
 
 export default readDatabase;
