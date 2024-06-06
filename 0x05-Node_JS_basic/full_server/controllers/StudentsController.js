3412abc10fead11e63b40ede932b2dd1181117ce
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 21:25:32 +0100

    Update StudentsController.js file

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,39 +1,61 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-  static async getAllStudents(req, res) {
-    try {
-      const students = await readDatabase(process.argv[2]);
-      const response = ['This is the list of our students'];
-      Object.keys(students).sort().forEach((field) => {
-        response.push(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
+  static getAllStudents(_req, res) {
+    const data = process.argv.length > 2 ? process.argv[2] : '';
+
+    readDatabase(data)
+      .then((students) => {
+        const responseMessages = ['This is the list of our students'];
+        const sortStudentsByName = (a, b) => {
+          if (a[0].toLowerCase() < b[0].toLowerCase()) {
+            return -1;
+          }
+          if (a[0].toLowerCase() > b[0].toLowerCase()) {
+            return 1;
+          }
+          return 0;
+        };
+
+        for (const [field, studentsList] of Object.entries(students).sort(sortStudentsByName)) {
+          responseMessages.push([
+            `Number of students in ${field}: ${studentsList.length}.`,
+            'List:',
+            studentsList.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        res.status(200).send(responseMessages.join('\n'));
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .send(err instanceof Error ? err.message : err.toString());
       });
-      res.status(200).send(response.join('\n'));
-    } catch (error) {
-      res.status(500).send('Cannot load the database');
-    }
   }
 
-  static async getAllStudentsByMajor(req, res) {
+  static getAllStudentsByMajor(req, res) {
+    const path = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = req.params;
 
-    // Validate major early and return if invalid
     if (!['CS', 'SWE'].includes(major)) {
-      return res.status(500).send('Major parameter must be CS or SWE');
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
     }
+    readDatabase(path)
+      .then((data) => {
+        let resText = '';
 
-    try {
-      const students = await readDatabase(process.argv[2]);
-      // Ensure a response is sent whether students are found or not
-      if (students[major] && students[major].length > 0) {
-        return res.status(200).send(`List: ${students[major].join(', ')}`);
-      }
-      // Always return even if the list is empty
-      return res.status(200).send('List: ');
-    } catch (error) {
-      // Make sure to catch any errors and return a response
-      return res.status(500).send('Cannot load the database');
-    }
+        if (Object.keys(data).includes(major)) {
+          const studentsList = data[major];
+          resText = `List: ${studentsList.map((student) => student.firstname).join(', ')}`;
+        }
+        res.status(200).send(resText);
+      })
+      .catch((error) => {
+        res
+          .status(500)
+          .send(error instanceof Error ? error.message : error.toString());
+      });
   }
 }
 
