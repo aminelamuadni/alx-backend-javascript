cfad9a4b76f13db186a18aa7b15f173f11e8c4da
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 17:02:27 +0100

    Update utils.js file

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -2,21 +2,23 @@ import fs from 'fs/promises';
 
 const readDatabase = async (path) => {
   try {
-    const data = await fs.readFile(path, 'utf-8');
+    const data = await fs.readFile(path, 'utf8');
     const lines = data.trim().split('\n').filter((line) => line);
-    const studentData = {};
+    const students = {};
 
     lines.slice(1).forEach((line) => {
-      const [firstname, , , field] = line.split(',');
-      studentData[field] = studentData[field] || [];
-      studentData[field].push(firstname);
+      const values = line.split(',').map((value) => value.trim());
+      const field = values[3];
+      if (!students[field]) {
+        students[field] = [];
+      }
+      students[field].push(values[0]);
     });
 
-    return studentData;
+    return students;
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 };
 
 export default readDatabase;
-module.exports = readDatabase;
