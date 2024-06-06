760b765727a1d4f040c481aa0877e553571c362f
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 19:30:15 +0100

    Update api.test.js file

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,27 +1,19 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('./api');
-
-const { expect } = chai;
-
-chai.use(chaiHttp);
+const request = require('request');
+const { expect } = require('chai');
 
 describe('index page', () => {
   it('should return the correct status code', () => new Promise((done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+    request.get('http://localhost:7865/', (_err, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      done();
+    });
   }));
 
   it('should return the correct result', () => new Promise((done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+    request.get('http://localhost:7865/', (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   }));
 });
