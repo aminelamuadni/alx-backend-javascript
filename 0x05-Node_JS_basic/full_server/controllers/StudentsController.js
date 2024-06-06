3ab9d2fa4aa3dea6e376a3dc3b80af6afae96320
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-06 17:02:39 +0100

    Update StudentsController.js file

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,14 +1,12 @@
 import readDatabase from '../utils';
 
-const VALID_MAJORS = ['CS', 'SWE'];
-
 class StudentsController {
   static async getAllStudents(req, res) {
     try {
-      const data = await readDatabase(process.argv[2]);
+      const students = await readDatabase(process.argv[2]);
       const response = ['This is the list of our students'];
-      Object.keys(data).sort().forEach((field) => {
-        response.push(`Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}`);
+      Object.keys(students).sort().forEach((field) => {
+        response.push(`Number of students in ${field}: ${students[field].length}. List: ${students[field].join(', ')}`);
       });
       res.status(200).send(response.join('\n'));
     } catch (error) {
@@ -19,27 +17,24 @@ class StudentsController {
   static async getAllStudentsByMajor(req, res) {
     const { major } = req.params;
 
-    // Check if the major is valid
-    if (!VALID_MAJORS.includes(major)) {
+    // Validate major early and return if invalid
+    if (!['CS', 'SWE'].includes(major)) {
       return res.status(500).send('Major parameter must be CS or SWE');
     }
 
     try {
-      const studentGroups = await readDatabase(process.argv[2]);
-
-      // Check if there are any students in the given major
-      if (studentGroups[major] && studentGroups[major].length > 0) {
-        // Return a list of students if found
-        const studentsList = studentGroups[major].map((student) => student.firstname).join(', ');
-        return res.status(200).send(`List: ${studentsList}`);
+      const students = await readDatabase(process.argv[2]);
+      // Ensure a response is sent whether students are found or not
+      if (students[major] && students[major].length > 0) {
+        return res.status(200).send(`List: ${students[major].join(', ')}`);
       }
-      // If no students are found for the major, return an empty 'List: '
+      // Always return even if the list is empty
       return res.status(200).send('List: ');
     } catch (error) {
+      // Make sure to catch any errors and return a response
       return res.status(500).send('Cannot load the database');
     }
   }
 }
 
 export default StudentsController;
-module.exports = StudentsController;
